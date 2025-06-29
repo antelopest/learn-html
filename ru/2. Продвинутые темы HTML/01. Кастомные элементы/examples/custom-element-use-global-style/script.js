b4c25f45afc2f5d44e488b47b0d201e055e9a27d
antelopest <antelopest@gmail.com>
2025-06-29 15:26:59 +0700

    main add file 05. Контроль загрузки ресурсов

--- ru/2. Продвинутые темы HTML/01. Кастомные элементы/examples/custom-element-use-global-style/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import initUserCard from './custom-elements/user-card/user-card.custom-element.js';
-import initJobCard from './custom-elements/job-card/job-card.custom-element.js';
-
-initUserCard();
-initJobCard();
